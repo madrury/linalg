649af14964a5b369c6029d0106c39be44639734b
madrury <matthew.drury.83@gmail.com>
2016-05-26 12:26:50 -0700

    Minor corrections and TODOs in vector.c

--- vector.c
+++ vector.c
@@ -14,9 +14,9 @@ struct vector* vector_new(int length) {
     assert(length >= 0);
 
     struct vector* new_vector = malloc(sizeof(struct vector));
-    check_memory((void*)new_vector);
+    check_memory((void*) new_vector);
 
-    DATA(new_vector) = malloc((sizeof(double))*length);
+    DATA(new_vector) = malloc((sizeof(double)) * length);
     check_memory((void*) DATA(new_vector));
 
     new_vector->length = length;
@@ -29,6 +29,7 @@ struct vector* vector_new(int length) {
 
 struct vector* vector_new_view(struct linalg_obj* parent, double* view, int length) {
     assert(length >= 0);
+    // TODO: Make this view check work.
     /* Check that pointers to the beginning and end of view vector live
        within the data segment of the parent object.
 
@@ -65,7 +66,7 @@ struct vector* vector_from_array(double* data, int length) {
 void vector_free(struct vector* v) {
     struct linalg_obj* mem_owner;
     if(OWNS_MEMORY(v)) {
-        if(REF_COUNT(v)== 0) {
+        if(REF_COUNT(v) == 0) {
             free(DATA(v));
             free(v);
         } else {
@@ -92,6 +93,7 @@ void vector_free_many(int n_to_free, ...) {
     }
 }
 
+/* Construct a vector of a given length filled with zeros. */
 struct vector* vector_zeros(int length) {
     struct vector* v = vector_new(length);
     for(int i = 0; i < v->length; i++) {
@@ -120,12 +122,14 @@ struct vector* vector_linspace(int length, double min, double max) {
 */
 struct vector* vector_slice(struct vector* v, int begin_idx, int end_idx) {
     assert(begin_idx <= end_idx);
+    // TODO: Make sure the slice does not exceed the bounds of the parent vector.
     int new_vector_length = end_idx - begin_idx;
     double* begin_ptr = DATA(v) + begin_idx;
     struct vector* w = vector_new_view((struct linalg_obj*) v, begin_ptr, new_vector_length);
     return w;
 }
 
+/* Copy all the data in a given vector into a new vector. */
 struct vector* vector_copy(struct vector* v) {
     struct vector* w = vector_new(v->length);
     for(int i = 0; i < v->length; i++) {
@@ -134,6 +138,20 @@ struct vector* vector_copy(struct vector* v) {
     return w;
 }
 
+
+/* Arithmatic methods.
+    
+    Each of the following methods implements an arithmetic operation on vectors.
+  Each comes in two flavors, anaoagous to the + and += operators:
+
+    - vector_operation(v1, v2) applies the given operation elementwise to pairs
+        taken from v1, v2, and stores the results in a *new* vector, which is
+        then returned.
+    - vector_operation_into(v1, v2) applies the given operation elementwise to
+        pairs taken from v1, v2, and stores the results in v1.  Note that this
+        operation destroys the data in v1, and if v1 is a reference, will
+        mutate the referenced object.
+*/
 struct vector* vector_subtract(struct vector* v1, struct vector* v2) {
     assert(vector_lengths_equal(v1, v2));
     struct vector* v = vector_new(v1->length);
@@ -192,8 +210,15 @@ struct vector* vector_scalar_multiply(struct vector* v, double s) {
     return w;
 }
 
+void vector_scalar_multiply_into(struct vector* v, double s) {
+    for(int i = 0; i < v->length; i++) {
+        VECTOR_IDX_INTO(v, i) = VECTOR_IDX_INTO(v, i) * s;
+    }
+}
+
+/* Check that two vectors are equal to within some additive tolerance. */
 bool vector_equal(struct vector* v1, struct vector* v2, double tol) {
-    if(v1->length != v2->length) {
+    if(!vector_lengths_equal(v1, v2)) {
         return false;
     }
     for(int i = 0; i < v1->length; i++) {
@@ -204,10 +229,8 @@ bool vector_equal(struct vector* v1, struct vector* v2, double tol) {
     return true;
 }
 
-void vector_scalar_multiply_into(struct vector* v, double s) {
-    for(int i = 0; i < v->length; i++) {
-        VECTOR_IDX_INTO(v, i) = VECTOR_IDX_INTO(v, i) * s;
-    }
+bool vector_lengths_equal(struct vector* v1, struct vector* v2) {
+    return (v1->length == v2->length);
 }
 
 double vector_dot_product(struct vector* v1, struct vector* v2) {
@@ -224,6 +247,11 @@ double vector_norm(struct vector* v) {
     return sqrt(norm_squared);
 }
 
+/* Print a vector to the console like:
+    [1, 2, 3, 4]
+*/
+// TODO: Maybe this should return a string, we are computing the representation and
+// displaying it in the same method.
 void vector_print(struct vector* v) {
     if(v->length == 0) {
         printf("[]\n");
@@ -239,6 +267,3 @@ void vector_print(struct vector* v) {
     }
 }
 
-bool vector_lengths_equal(struct vector* v1, struct vector* v2) {
-    return (v1->length == v2->length);
-}
